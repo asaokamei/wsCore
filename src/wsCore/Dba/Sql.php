0df418f3042f9794f8237a6d1359e81309ca5e4c
Asao <kamei@workspot.jp>
2012-09-06 23:22:15 +0900

    added bunches of Sql methods.

--- src/wsCore/Dba/Sql.php
+++ src/wsCore/Dba/Sql.php
@@ -12,8 +12,8 @@ class Sql
     var $table;
     var $join = array();
     var $columns;
-    var $values;
-    var $functions;
+    var $values = array();
+    var $functions = array();
     var $order;
     var $where;
     var $group;
@@ -31,8 +31,148 @@ class Sql
         $this->dbConn = ( is_object( $dbConn ) ) ?: Pdo::connect( $dbConn );
     }
     // +----------------------------------------------------------------------+
-    public function prepVal( &$val ) {
-
+    public function prepValue( &$val ) {
+        if( is_array( $val ) ) {
+            foreach( $val as $k => $v ) {
+                $this->prepValue( $v );
+                $val[ $k ] = $v;
+            }
+        }
+        else {
+            $holder = ':db_prep_' . count( $this->prepared_values );
+            $this->prepared_values[ $holder ] = $val;
+            $val = $holder;
+        }
+        return $this;
+    }
+    public function quote( $val ) {
+        if( is_array( $val ) ) {
+            foreach( $val as $k => $v ) {
+                $val[ $k ] = $this->quote( $v );
+            }
+        }
+        else {
+            if( is_object( $this->dbConn ) ) {
+                $val = $this->dbConn->quote( $val );
+            }
+            $val = addslashes( $val );
+        }
+        return $val;
+    }
+    public function p( $val ) {
+        $this->prepValue( $val );
+        return $val;
+    }
+    public function q( &$val ) {
+        $val = '\'' . $this->quote( $val ) . '\'';
+        return $val;
+    }
+    // +----------------------------------------------------------------------+
+    public function table( $table ) {
+        $this->table = $table;
+        return $this;
+    }
+    public function column( $column ) {
+        $this->columns = $column;
+        return $this;
+    }
+    public function values( $values ) {
+        $this->values = $values;
+        return $this;
+    }
+    public function order( $order ) {
+        $this->order = $order;
+        return $this;
+    }
+    public function group( $group ) {
+        $this->group = $group;
+        return $this;
+    }
+    public function limit( $limit ) {
+        $this->limit  = ( $limit  ) ? $limit : FALSE;
+        return $this;
+    }
+    public function offset( $offset, $limit=FALSE ) {
+        $this->offset = ( is_numeric( $offset ) ) ? $offset: 0;
+        return $this;
+    }
+    public function distinct(){
+        $this->distinct = TRUE;
+        return $this;
+    }
+    public function join( $table, $join, $by=NULL, $columns=NULL ) {
+        $this->join[] = "{$join} {$table}" . ($by)? " {$by}( {$columns} )": '';
+        return $this;
+    }
+    public function joinUsing( $table, $columns ) {
+        return $this->join( $table, 'JOIN', 'USING', $columns );
+    }
+    public function joinLeftUsing( $table, $columns ) {
+        return $this->join( $table, 'LEFT JOIN', 'ON', $columns );
+    }
+    public function joinOn( $table, $columns ) {
+        return $this->join( $table, 'JOIN', 'USING', $columns );
+    }
+    public function joinLeftOn( $table, $columns ) {
+        return $this->join( $table, 'LEFT JOIN', 'ON', $columns );
+    }
+    public function where( $col, $val, $rel='=', $op='' ) {
+        $where = array( 'col' => $col, 'val'=> $val, 'rel' => $rel, 'op' => $op );
+        $this->where[] = $where;
+        return $this;
+    }
+    public function setWhere( $where ) {
+        $this->where = $where;
+        return $this;
+    }
+    // +----------------------------------------------------------------------+
+    public function makeWhere( $whereList ) {
+        if( is_array( $this->where ) ) {
+            $where = '';
+            foreach( $this->where as $wh ) {
+                $where .= call_user_func_array( array( $this, 'formWhere' ), $wh );
+            }
+        }
+        else {
+            $where = $this->where;
+        }
+        $where = trim( $where );
+        preg_replace( '/^(AND|OR)/i', '', $where );
+        return $where;
+    }
+    public function formWhere( $col, $val, $rel='=', $op='AND' ) {
+        $where = '';
+        $rel = strtoupper( $rel );
+        if( $rel == 'IN' ) { 
+            $val = "( " . is_array( $val ) ? implode( ", ", $val ): "{$val}" . " )";
+        }
+        elseif( $rel = 'BETWEEN' ) {
+            $val = "{$val{0}} AND {$val{1}}";
+        }
+        elseif( $col == '(' ) {
+            $val = $rel = '';
+        }
+        elseif( $col == ')' ) {
+            $op = $rel = $val = '';
+        }
+        $where .= trim( "{$op} {$col} {$rel} {$val}" ) . ' ';
+        return $where;
+    }
+    public function processValues() {
+        if( !empty( $this->values ) )
+        foreach( $this->values as $key => $val ) {
+            if( $val === NULL ) {
+                $this->functions[ $key ] = 'NULL';
+                unset( $this->values[ $key ] );
+            }
+        }
+        return array_merge( $this->functions, $this->p( $this->values ) );
+    }
+    public function makeInsert() {
+        $values = $this->processValues();
+        $listV = implode( ', ', $values );
+        $listC = implode( ', ', array_keys( $values ) );
+        return "INSERT INTO {$this->table} ( {$listC} ) VALUES ( {$listV} )";
     }
     // +----------------------------------------------------------------------+
     public function execSQL( $sql=NULL )
@@ -71,26 +211,12 @@ class Sql
         $this->dbStmt->execute( $prepared_values );
         return $this;
     }
-    public function limit( $limit, $offset=FALSE ) {
-        $this->limit  = ( $limit  ) ? $limit : FALSE;
-        $this->offset = ( is_numeric( $offset ) ) ? $offset: 0;
-        return $this;
-    }
-    public function offset( $offset, $limit=FALSE ) {
-        return $this->limit( $limit, $offset );
-    }
     public function setFetchMode( $mode, $class=NULL ) {
         $this->dbStmt->setFetchMode( $mode, $class );
         $this->fetchMode = $mode;
         return $this;
     }
     // +----------------------------------------------------------------------+
-    public function quote( $val ) {
-        if( is_object( $this->dbConn ) ) {
-            return $this->dbConn->quote( $val );
-        }
-        return addslashes( $val );
-    }
     public function lastId() {
         return $this->dbConn->lastInsertId();
     }
