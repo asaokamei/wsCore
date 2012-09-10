bb0433b57ea89d162a0aeba5ce51dfb0d8b71998
asao Kamei <asao@ubuPC.(none)>
2012-09-10 11:53:47 +0900

    use use to simplify namespaced Rdb calling sequence.

--- src/wsTests/Dba/Rdb_MySql_Test.php
+++ src/wsTests/Dba/Rdb_MySql_Test.php
@@ -1,5 +1,6 @@
 <?php
 namespace wsTests\Dba;
+use \wsCore\Dba\Rdb as Rdb;
 
 require_once( __DIR__ . '/../../autoloader.php' );
 
@@ -12,7 +13,7 @@ class Dba_Rdb_MySql_Test extends \PHPUnit_Framework_TestCase
         $this->config = array(
             'dsn' => 'db=mysql dbname=test_wsCore username=admin password=admin'
         );
-        \wsCore\Dba\Rdb::set( 'config', $this->config );
+        Rdb::set( 'config', $this->config );
     }
     public function test_1()
     {
@@ -23,7 +24,7 @@ class Dba_Rdb_MySql_Test extends \PHPUnit_Framework_TestCase
      */
     public function test_bad_sql_statement()
     {
-        $pdo = \wsCore\Dba\Rdb::connect( 'config' );
+        $pdo = Rdb::connect( 'config' );
         $test = "CREATE TABLE test ( id int ) is a bad sql ;";
         $pdo->query( $test );
     }
@@ -31,12 +32,12 @@ class Dba_Rdb_MySql_Test extends \PHPUnit_Framework_TestCase
     public function test_connection_to_wsCore_db()
     {
         // should not throw any exceptions.
-        $pdo = \wsCore\Dba\Rdb::connect( 'config' );
+        $pdo = Rdb::connect( 'config' );
     }
     // +----------------------------------------------------------------------+
     public function test_mysql_driver_name()
     {
-        $pdo = \wsCore\Dba\Rdb::connect( 'config' );
+        $pdo = Rdb::connect( 'config' );
         $db  = $pdo->getAttribute( \PDO::ATTR_DRIVER_NAME );
         $this->assertEquals( 'mysql', $db );
     }
@@ -49,8 +50,8 @@ class Dba_Rdb_MySql_Test extends \PHPUnit_Framework_TestCase
         $badDsn = array(
             'dsn' => 'db=noDb dbname=test username=admin password=admin'
         );
-        \wsCore\Dba\Rdb::set( 'badDsn', $badDsn );
-        $pdo = \wsCore\Dba\Rdb::connect( 'badDsn' );
+        Rdb::set( 'badDsn', $badDsn );
+        $pdo = Rdb::connect( 'badDsn' );
     }
     // +----------------------------------------------------------------------+
 }
