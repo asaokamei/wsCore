47a3bce87443945e626c08aa81c550b4582f666b
asao Kamei <asao@ubuPC.(none)>
2012-09-10 11:39:29 +0900

    test on driver name, throwing PDOException.

--- src/wsTests/Dba/Rdb_MySql_Test.php
+++ src/wsTests/Dba/Rdb_MySql_Test.php
@@ -14,14 +14,31 @@ class Dba_Rdb_MySql_Test extends \PHPUnit_Framework_TestCase
         );
         \wsCore\Dba\Rdb::set( 'config', $this->config );
     }
-    // +----------------------------------------------------------------------+
     public function test_1()
     {
     }
     // +----------------------------------------------------------------------+
-    public function test_connection_to_wsCore_db()
+    /**
+     * @expectedException PDOException
+     */
+    public function test_bad_sql_statement()
     {
         $pdo = \wsCore\Dba\Rdb::connect( 'config' );
+        $test = "CREATE TABLE test ( id int ) is a bad sql ;";
+        $pdo->query( $test );
+    }
+    // +----------------------------------------------------------------------+
+    public function test_connection_to_wsCore_db()
+    {
+        // should not throw any exceptions.
+        $pdo = \wsCore\Dba\Rdb::connect( 'config' );
+    }
+    // +----------------------------------------------------------------------+
+    public function test_mysql_driver_name()
+    {
+        $pdo = \wsCore\Dba\Rdb::connect( 'config' );
+        $db  = $pdo->getAttribute( \PDO::ATTR_DRIVER_NAME );
+        $this->assertEquals( 'mysql', $db );
     }
     // +----------------------------------------------------------------------+
     /**
