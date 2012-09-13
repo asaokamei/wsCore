b36748af7ba541e825a8d7c5dbacf2beef29e0a0
asao Kamei <asao@ubuPC.(none)>
2012-09-13 14:28:15 +0900

    test on Dimplet. works great cos it's mostly Pimple's code.

--- src/wsTests/DiContainer/Dimple_Test.php
+++ src/wsTests/DiContainer/Dimple_Test.php
@@ -14,6 +14,120 @@ class Dimple_Test extends \PHPUnit_Framework_TestCase
         $this->container = new Dimplet();
     }
 
+    /**
+     * @param string $text
+     * @return callable
+     */
+    public function getMaker( $text='test closure maker' )
+    {
+        return function($c) use( $text ) {
+            $obj = new \stdClass();
+            $obj->name = $text;
+            return $obj;
+        };
+    }
+    // +----------------------------------------------------------------------+
+    /**
+     *
+     */
+    public function test_protect_closure()
+    {
+        // set up closure for testing: maker.
+        $text  = 'test closure maker';
+        $maker = $this->getMaker( $text );
+        $maker = $this->container->protect( $maker ); // protect the maker.
+        $this->container->set( 'maker', $maker );
+        $func = $this->container->get( 'maker' );
+        $this->assertEquals( $maker, $func );
+    }
+    /**
+     *
+     */
+    public function test_raw_closure()
+    {
+        // set up closure for testing: maker.
+        $text  = 'test closure maker';
+        $maker = $this->getMaker( $text );
+        $this->container->set( 'maker', $maker );
+        $raw = $this->container->raw( 'maker' );
+
+        $this->assertEquals( $maker, $raw );
+    }
+    /**
+     *
+     */
+    public function test_share_closure()
+    {
+        // set up closure for testing: maker.
+        $text  = 'test closure maker';
+        $maker = $this->getMaker( $text );
+        $maker = $this->container->share( $maker ); // share the maker.
+        $this->container->set( 'maker', $maker );
+
+        // get maker from container.
+        $obj = $this->container->get( 'maker' );
+        $this->assertEquals( $text, $obj->name );
+
+        // get fresh maker. still identical to obj cos share is used.
+        $obj2 = $this->container->fresh( 'maker' );
+        $this->assertEquals( $text, $obj2->name );
+        $this->assertEquals( $obj, $obj2 );
+        $this->assertSame( $obj, $obj2 );
+    }
+
+    /**
+     *
+     */
+    public function test_extend_closure()
+    {
+        // set up closure for testing: maker.
+        $text  = 'test closure maker';
+        $maker = $this->getMaker( $text );
+        $this->container->set( 'maker', $maker );
+
+        // extend the maker. set another attribute.
+        $text2 = 'extending maker';
+        $extend = function( $maker, $c ) use( $text2 ) {
+            $maker->name2 = $text2;
+            return $maker;
+        };
+        $this->container->extend( 'maker', $extend );
+
+        // get maker from container.
+        $obj = $this->container->get( 'maker' );
+        $this->assertEquals( $text, $obj->name );
+        $this->assertEquals( $text2, $obj->name2 );
+    }
+
+    /**
+     *
+     */
+    public function test_closure()
+    {
+        // set up closure for testing: maker.
+        $text  = 'test closure maker';
+        $maker = $this->getMaker( $text );
+        $this->container->set( 'maker', $maker );
+
+        // get maker from container.
+        $obj = $this->container->get( 'maker' );
+        $this->assertEquals( $text, $obj->name );
+
+        // get fresh maker. they are different object.
+        $obj2 = $this->container->fresh( 'maker' );
+        $this->assertEquals( $text, $obj2->name );
+        $this->assertEquals( $obj, $obj2 );
+        $this->assertNotSame( $obj, $obj2 );
+
+        // get existing (i.e. first) maker.
+        $obj3 = $this->container->get( 'maker' );
+        $this->assertEquals( $obj, $obj3 );
+        $this->assertSame( $obj, $obj3 );
+    }
+
+    /**
+     *
+     */
     public function test_get_and_fresh()
     {
         $invoice1 = $this->container->get( '\wsTests\DiContainer\DimpleMockBiz\Invoice' );
