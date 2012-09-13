afdb8cf6a92a12663f27cfc87aa8b663b3dde6fb
asao Kamei <asao@ubuPC.(none)>
2012-09-13 12:29:34 +0900

    supressed all warnings from PhpStorm. Closure is global class.

--- src/wsCore/DiContainer/Dimplet.php
+++ src/wsCore/DiContainer/Dimplet.php
@@ -15,14 +15,14 @@ class Dimplet
     /**
      * Sets a parameter or an object.
      *
-     * Objects must be defined as Closures.
+     * Objects must be defined as \Closures.
      *
      * Allowing any PHP callable leads to difficult to debug problems
      * as function names (strings) are callable (creating a function with
      * the same a name as an existing parameter would break your container).
      *
      * @param string $id    The unique identifier for the parameter or object
-     * @param mixed  $value The value of the parameter or a closure to defined an object
+     * @param mixed  $value The value of the parameter or a \Closure to defined an object
      */
     public function set($id, $value)
     {
@@ -31,7 +31,7 @@ class Dimplet
 
     /**
      * gets id from container. id can be:
-     *  - a pre-set id for a data or a factory closure.
+     *  - a pre-set id for a data or a factory \Closure.
      *  - a class name to construct.
      * 
      * @param $id
@@ -59,7 +59,7 @@ class Dimplet
         if( array_key_exists($id, $this->values) ) {
             $found = $this->values[$id];
             /** @var $found callable */
-            $found = ( $found instanceof Closure ) ? $found( $this ) : $found;
+            $found = ( $found instanceof \Closure ) ? $found( $this ) : $found;
         }
         elseif( class_exists( $id ) ) {
             // construct the class, and inject via interfaces.
@@ -93,13 +93,13 @@ class Dimplet
     }
     /**
      * from Pimple!
-     * Returns a closure that stores the result of the given closure for
+     * Returns a \Closure that stores the result of the given \Closure for
      * uniqueness in the scope of this instance of Pimple.
      *
-     * @param Closure $callable A closure to wrap for uniqueness
-     * @return Closure The wrapped closure
+     * @param \Closure $callable A \Closure to wrap for uniqueness
+     * @return \Closure The wrapped \Closure
      */
-    public function share( Closure $callable )
+    public function share( \Closure $callable )
     {
         return function ($c) use ($callable) {
             static $object;
@@ -116,10 +116,10 @@ class Dimplet
      * Protects a callable from being interpreted as a service.
      * This is useful when you want to store a callable as a parameter.
      *
-     * @param Closure $callable A closure to protect from being evaluated
-     * @return Closure The protected closure
+     * @param \Closure $callable A \Closure to protect from being evaluated
+     * @return \Closure The protected \Closure
      */
-    public function protect(Closure $callable)
+    public function protect( \Closure $callable)
     {
         return function ($c) use ($callable) {
             return $callable;
@@ -128,10 +128,10 @@ class Dimplet
 
     /**
      * from Pimple!
-     * Gets a parameter or the closure defining an object.
+     * Gets a parameter or the \Closure defining an object.
      *
      * @param string $id The unique identifier for the parameter or object
-     * @return mixed The value of the parameter or the closure defining an object
+     * @return mixed The value of the parameter or the \Closure defining an object
      */
     public function raw($id)
     {
@@ -145,11 +145,11 @@ class Dimplet
      * without necessarily loading that object.
      *
      * @param string  $id       The unique identifier for the object
-     * @param Closure $callable A closure to extend the original
+     * @param \Closure $callable A \Closure to extend the original
      * @throws \RuntimeException
-     * @return Closure The wrapped closure
+     * @return \Closure The wrapped \Closure
      */
-    public function extend($id, Closure $callable)
+    public function extend($id, \Closure $callable)
     {
         if (!array_key_exists($id, $this->values)) {
             throw new \RuntimeException( sprintf('Identifier "%s" is not defined.', $id ) );
@@ -157,10 +157,11 @@ class Dimplet
 
         $factory = $this->values[$id];
 
-        if (!($factory instanceof Closure)) {
+        if (!($factory instanceof \Closure)) {
             throw new \RuntimeException( sprintf('Identifier "%s" does not contain an object definition.', $id ) );
         }
 
+        /** @var $factory \Closure */
         return $this->values[$id] = function ($c) use ($callable, $factory) {
             return $callable($factory($c), $c);
         };
