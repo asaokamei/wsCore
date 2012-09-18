810481da4913d996d63c1341f3d362da1ebba011
asao Kamei <asao@ubuPC.(none)>
2012-09-18 14:39:50 +0900

    added test on validating an array.

--- src/wsTests/Validator/DataIO_Test.php
+++ src/wsTests/Validator/DataIO_Test.php
@@ -22,6 +22,17 @@ class DataIO_Test extends \PHPUnit_Framework_TestCase
         );
     }
     // +----------------------------------------------------------------------+
+    public function test_validating_array()
+    {
+        $input = array( 'num' => array( '1', '2', 'bad', '4' ) );
+        $err_msg = 'Not a Number';
+        $errors = FALSE;
+        $this->dio->source( $input );
+        $this->dio->pushValue( 'num', 'pattern:number | err_msg:'.$err_msg );
+        $isError = $this->dio->popErrors( $errors );
+        $this->assertTrue( !!$isError );
+        $this->assertEquals( $err_msg, $errors['num'][2] );
+    }
     public function test_simple_push_and_pop()
     {
         $data = $this->getData();
