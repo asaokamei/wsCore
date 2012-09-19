e79c48cd0ca0f4a9eeb3396313fdf715dad29c5e
asao Kamei <asao@ubuPC.(none)>
2012-09-19 11:22:57 +0900

    added a test for replacing original value.

--- sandBox/Tags_Test.php
+++ sandBox/Tags_Test.php
@@ -8,8 +8,10 @@ echo $tags()->a( 'a link' )->href( 'do.php' )->target( '_blank' );
 echo $tags( 'a', 'a link' )->href( 'do.php' )->target( '_blank' );
 
 echo $tags()->a( 'a link' )->href( 'do.php' )->target( '_blank' )->_class( 'myClass' )->_class( 'myClass2' );
+echo $tags()->a( 'a link' )->href( 'do.php' )->target( '_blank' )->_class( 'myClass' )->_class( 'myClass2', FALSE );
 
-echo $tags( 'a', 'a link' )->href( 'do.php' )->target( '_blank' )->_style( 'style1' )->_style( 'style2' );
+echo $tags( 'a', 'a link' )->href( 'do.php' )->target( '_blank' )->style( 'style1' )->style( 'style2' );
+echo $tags( 'a', 'a link' )->href( 'do.php' )->target( '_blank' )->style( 'style1' )->_style( 'style2', FALSE );
 
 echo $tags()->div(
     'this is a text',
@@ -18,7 +20,7 @@ echo $tags()->div(
     $tags()->a( 'a link' )->href( 'do2.php' )->target( '_blank' )
 );
 
-echo $tags()->div()->_contain(
+echo $tags()->div()->_class( 'divClass' )->_contain(
     'this is a text',
     $tags()->div(
         $tags()->a( 'a link' )->href( 'do.php' )->target( '_blank' ),
