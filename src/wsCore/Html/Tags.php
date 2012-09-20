2ddc91844b4049282c12d7f3035b6391270c4545
asao Kamei <asao@ubuPC.(none)>
2012-09-20 09:26:34 +0900

    textarea is not in-line tag (well, inside is one line...).

--- src/wsCore/Html/Tags.php
+++ src/wsCore/Html/Tags.php
@@ -28,7 +28,7 @@ class Tags
     );
     /** @var array                  in-line tags   */
     public static $tag_span = array(
-        'span', 'p', 'strong', 'i', 'sub', 'textarea', 'li', '',
+        'span', 'p', 'strong', 'i', 'sub', 'li', '',
     );
     /** @var array                  how to connect attribute values */
     public static $attribute_connectors = array(
