f817a54159c98eee99dc0b3d6816b7174a3c99ea
asao Kamei <asao@ubuPC.(none)>
2012-09-21 14:05:30 +0900

    Old Upload and Csv re-written.

---
+++ sandBox/Csv.php
@@ -0,0 +1,106 @@
+<?php
+
+class Csv
+{
+    public static $toEncoding = 'UTF-8';
+
+    private $fromEncoding = 'SJIS-win';
+
+    private $filename = NULL;
+
+    private $filePointer = NULL;
+
+    private $header = array();
+
+    private $useHeader = FALSE;
+
+    /**
+     * set
+     * @param null $filename
+     * @param string $from
+     */
+    public function __construct( $filename=NULL, $from=NULL )
+    {
+        $this->filename = $filename;
+        if( isset( $from ) ) $this->fromEncoding = $from;
+    }
+
+    /**
+     * opens file. specify encoding of the CSV file.
+     *
+     * @param string|null $filename
+     * @param string|null $from
+     * @throws RuntimeException
+     * @return resource
+     */
+    public function open( $filename=NULL, $from=NULL )
+    {
+        $filename = ( $filename ) ?: $this->filename;
+        $from     = ( $from )     ?: $this->fromEncoding;
+        if( !$filename ) throw new RuntimeException( "CSV file does not exist: ".$filename );
+        if( $from == static::$toEncoding ) {
+            $fp = fopen( $filename, 'r+' );
+        }
+        else {
+            $fp = $this->fOpenAsUtf8( $filename, $from );
+        }
+        $this->filePointer = $fp;
+        return $fp;
+    }
+
+    /**
+     * gets CSV data from file in UTF-8.
+     *
+     * @param int $size
+     * @throws RuntimeException
+     * @return array
+     */
+    public function getCsv( $size=10280 )
+    {
+        if( !$this->filePointer ) throw new RuntimeException( "Invalid CSV pointer: " );
+        $data = fgetcsv( $this->filePointer, $size );
+        if( $this->useHeader ) {
+            $result = array();
+            foreach( $data as $col => $val ) {
+                $result[ $this->header[$col] ] = $val;
+            }
+            $data = $result;
+        }
+        return $data;
+    }
+
+    /**
+     * gets CSV data as header.
+     *
+     * @param int $size
+     * @param bool $useHeader
+     * @return array
+     */
+    public function getHeader( $size=10280, $useHeader=FALSE )
+    {
+        $header = $this->getCsv( $size );
+        $this->useHeader = $useHeader;
+        if( $this->useHeader ) {
+            $this->header = $header;
+        }
+        return $header;
+    }
+
+    /**
+     * open file as utf-8 encoding.
+     *
+     * @param $file_name
+     * @param string $from
+     * @return resource
+     */
+    public static function fOpenAsUtf8( $file_name, $from='SJIS-win' )
+    {
+        $data = file_get_contents( $file_name );
+        $data = mb_convert_encoding( $data, static::$toEncoding, $from );
+        $fp   = fopen( 'php://memory', 'r+' );
+        fwrite( $fp, $data );
+        rewind( $fp ); //
+        return $fp;
+    }
+
+}
---
+++ sandBox/Upload.php
@@ -0,0 +1,151 @@
+<?php
+
+class Upload
+{
+    const ERR_NO_UPLOAD = 'ERR901';
+    const ERR_INVALID   = 'ERR902';
+    const ERR_SIZE_ZERO = 'ERR903';
+    const ERR_BAD_EXT   = 'ERR904';
+
+    /** @var array        error messages */
+    private $errorMessages = array();
+
+    /** @var array        information about uploaded file */
+    private $upload_info = array();
+
+    /** @var bool         true if error happens */
+    private $error      = FALSE;
+
+    /** @var null         error code if error */
+    private $error_code = NULL;
+
+    /**
+     * @param null $userFile
+     */
+    public function __construct( $userFile=NULL )
+    {
+        $this->setErrorMessage();
+        if( $userFile ) $this->uploadFile( $userFile );
+    }
+
+    /**
+     * setup error messages.
+     */
+    public function setErrorMessage()
+    {
+        $this->errorMessages = array(
+            UPLOAD_ERR_OK         =>  'リフレッシュなどでファイルがアップされてません。 ',
+            UPLOAD_ERR_INI_SIZE   =>  'ファイルサイズが上限を超えています。  ',
+            UPLOAD_ERR_FORM_SIZE  =>  'ファイルサイズが上限値を超えています。  ',
+            UPLOAD_ERR_PARTIAL    =>  'アップロードが完了しませんでした。  ',
+            UPLOAD_ERR_NO_FILE    =>  'ファイルはアップロードされませんでした。  ',
+            UPLOAD_ERR_NO_TMP_DIR =>  'テンポラリフォルダがありません。 ',
+            UPLOAD_ERR_CANT_WRITE =>  'ディスクへの書き込みに失敗しました。 ',
+            UPLOAD_ERR_EXTENSION  =>  'アップロードが拡張モジュールによって停止されました。  ',
+            static::ERR_NO_UPLOAD =>  'ファイルがアップロードされていません。 ',
+            static::ERR_NO_UPLOAD =>  'アップロードされたファイルではありません。 ',
+            static::ERR_SIZE_ZERO =>  'ファイルサイズが空です（サイズ＝０）。 ',
+            static::ERR_BAD_EXT   =>  'ファイルの拡張子が正しくありません。 ',
+        );
+    }
+
+    /**
+     * sets user file name for uploaded file.
+     *
+     * @param $userFile
+     * @return bool
+     */
+    public function uploadFile( $userFile )
+    {
+        if( isset( $_FILES[ $userFile ] ) ) {
+            $this->upload_info = $_FILES[ $userFile ];
+        }
+        else {
+            $this->upload_info = array(
+                'error' => static::ERR_NO_UPLOAD,
+            );
+        }
+        if( isset( $this->upload_info[ 'error' ] ) ) {
+            $this->setError( $this->upload_info[ 'error' ] );
+        }
+        if( !isset( $this->upload_info[ 'size' ] ) || $this->upload_info[ 'size' ] <= 0 ) {
+            $this->setError( static::ERR_SIZE_ZERO );
+        }
+        if( !is_uploaded_file( $this->upload_info[ 'tmp_name' ] ) ) {
+            $this->setError( static::ERR_INVALID );
+        }
+        return $this->error;
+    }
+
+    /**
+     * @param $error_code
+     * @return bool
+     */
+    public function setError( $error_code ) {
+        $this->error_code = $error_code;
+        $this->error      = TRUE;
+        return $this->error;
+    }
+
+    /**
+     * saves temporary file to the folder with original file name.
+     *
+     * @param $folder
+     * @param bool $overwrite
+     * @return bool
+     */
+    public function save( $folder, $overwrite=TRUE )
+    {
+        $orig_name = $this->upload_info[ 'name' ];
+        return $this->saveAs( $folder . '/' . $orig_name, $overwrite );
+    }
+
+    /**
+     * saves temporary file to a specific file name.
+     *
+     * @param $filename
+     * @param bool $overwrite
+     * @return bool
+     */
+    public function saveAs( $filename, $overwrite=TRUE )
+    {
+        if( $this->error ) return FALSE;
+        $tmp_name = $this->upload_info[ 'tmp_name' ];
+        $ok = move_uploaded_file( $tmp_name, $filename );
+        return $ok;
+    }
+
+    /**
+     * returns valid (bool) and err code
+     *
+     * @param $err_code
+     * @return bool
+     */
+    public function isValid( &$err_code=NULL ) {
+        $err_code = $this->error_code;
+        return !$this->error;
+    }
+
+    /**
+     * checks file extension of the original file name.
+     *
+     * @param $extension
+     * @return bool
+     */
+    public function verifyExt( $extension )
+    {
+        if( $this->error ) return FALSE;
+        if( !is_array( $extension ) ) $extension = array( $extension );
+        $error = TRUE;
+        foreach( $extension as $ext ) {
+            if( $ext == pathinfo( $this->upload_info[ 'name' ], PATHINFO_EXTENSION ) ) {
+                $error = FALSE;
+                break;
+            }
+        }
+        if( $error ) {
+            $this->setError( static::ERR_BAD_EXT );
+        }
+        return !$this->error;
+    }
+}
