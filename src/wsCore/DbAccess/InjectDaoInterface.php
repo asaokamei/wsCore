979336289a446467eb15bd1d3c3e956945d093da
asao Kamei <asao@ubuPC.(none)>
2012-09-24 14:02:53 +0900

    removed obsolete interfaces for setter injection.

--- src/wsCore/DbAccess/InjectDaoInterface.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace wsCore\DbAccess;
-
-interface InjectDaoInterface {}
--- src/wsCore/DbAccess/InjectPdoInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace wsCore\DbAccess;
-
-interface InjectPdoInterface
-{
-    public function injectPdo( $pdo );
-}
