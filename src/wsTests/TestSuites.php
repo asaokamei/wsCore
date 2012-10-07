aed0d58c7b6fe7c61ddff93b4e1612c174241663
Asao <kamei@workspot.jp>
2012-10-07 12:03:50 +0900

    added more tests.

--- src/wsTests/TestSuites.php
+++ src/wsTests/TestSuites.php
@@ -9,11 +9,22 @@ class wsCore_SuiteTests
     {
         $suite = new \PHPUnit_Framework_TestSuite( 'all tests for wsCore\'s' );
         $folder = __DIR__ . '/';
+        
+        // DbAccess Tests
         $suite->addTestFile( $folder . 'DbAccess/DbAccess_SuiteTest.php' );
+        
+        // Validator Tests
+        $suite->addTestFile( $folder . 'Validator/Validator_Test.php' );
+        $suite->addTestFile( $folder . 'Validator/DataIO_Test.php' );
+        
+        // wait until Dimplet test rewrite. 
         //$suite->addTestFile( $folder . 'DiContainer/Dimple_Test.php' );
+        
+        // Html Tests
         $suite->addTestFile( $folder . 'Html/Form_Test.php' );
-        $suite->addTestFile( $folder . 'Html/Selector_Test.php' );
         $suite->addTestFile( $folder . 'Html/Tags_Test.php' );
+        // wait until Selector test uses PHPUnit. 
+        //$suite->addTestFile( $folder . 'Html/Selector_Test.php' );
         return $suite;
     }
 }
