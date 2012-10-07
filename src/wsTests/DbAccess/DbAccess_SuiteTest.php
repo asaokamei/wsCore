026a8e3470ee3ab381c7ee456004c89c44e031db
Asao <kamei@workspot.jp>
2012-10-07 17:41:51 +0900

    DbAccess Suites has DataRecord test.

--- src/wsTests/DbAccess/DbAccess_SuiteTest.php
+++ src/wsTests/DbAccess/DbAccess_SuiteTest.php
@@ -11,6 +11,7 @@ class DbAccess_SuiteTests
         $suite->addTestFile( $folder . 'Rdb_Test.php' );
         $suite->addTestFile( $folder . 'Query_Test.php' );
         $suite->addTestFile( $folder . 'Dao_MySql_Test.php' );
+        $suite->addTestFile( $folder . 'DataRecord_MySql_Test.php' );
         return $suite;
     }
 }
