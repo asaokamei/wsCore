ac1daf9319dc08f32ab13bddf1ac217024d19f02
Asao <kamei@workspot.jp>
2012-10-15 08:11:50 +0900

    copied methods for Contact data. not modified yet.

--- src/wsTests/DbAccess/Dao/SetUp.php
+++ src/wsTests/DbAccess/Dao/SetUp.php
@@ -32,7 +32,11 @@ class Dao_SetUp
         ";
         return $sql;
     }
-    
+
+    /**
+     * @param int $idx
+     * @return array
+     */
     static function makeFriend( $idx=0 )
     {
         $values = array(
@@ -45,4 +49,43 @@ class Dao_SetUp
         }
         return $values;
     }
+    
+    /**
+     * @param string $table
+     * @return string
+     */
+    static function setupContact( $table='contact' )
+    {
+        $sql = "
+            CREATE TABLE {$table} (
+              friend_id    SERIAL, 
+              friend_name  text, 
+              friend_bday  date,
+              new_dt_friend   datetime,
+              mod_dt_friend   datetime,
+              constraint friend_pkey PRIMARY KEY (
+                friend_id
+              )
+            )
+        ";
+        return $sql;
+    }
+    
+    /**
+     * @param int $idx
+     * @return array
+     */
+    static function makeContact( $idx=0 )
+    {
+        $values = array(
+            'friend_name' => 'my friend',
+            'friend_bday' => '1980-01-23',
+        );
+        if( $idx > 0 ) {
+            $values[ 'friend_name' ] .= '#' . $idx;
+            $values[ 'friend_bday' ]  = date( 'Y-m-d', mktime( 0,0,0, 1, 23+$idx, 1980 ) );
+        }
+        return $values;
+    }
 }
+    
