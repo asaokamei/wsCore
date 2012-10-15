a5b186719815763a3ea40c1179d80cf259d34478
Asao <kamei@workspot.jp>
2012-10-15 19:46:21 +0900

    added contact table creation and deletion.

--- src/wsTests/DbAccess/Dao/SetUp.php
+++ src/wsTests/DbAccess/Dao/SetUp.php
@@ -73,12 +73,12 @@ class Dao_SetUp
     {
         $sql = "
             CREATE TABLE {$table} (
-              friend_id    SERIAL, 
-              friend_name  text, 
-              friend_bday  date,
-              new_dt_friend   datetime,
-              mod_dt_friend   datetime,
-              constraint friend_pkey PRIMARY KEY (
+              contact_id       SERIAL, 
+              friend_id        int,
+              contact_info     text, 
+              new_dt_contact   datetime,
+              mod_dt_contact   datetime,
+              constraint contact_id PRIMARY KEY (
                 friend_id
               )
             )
@@ -93,12 +93,10 @@ class Dao_SetUp
     static function makeContact( $idx=0 )
     {
         $values = array(
-            'friend_name' => 'my friend',
-            'friend_bday' => '1980-01-23',
+            'contact_info' => 'my contact',
         );
         if( $idx > 0 ) {
-            $values[ 'friend_name' ] .= '#' . $idx;
-            $values[ 'friend_bday' ]  = date( 'Y-m-d', mktime( 0,0,0, 1, 23+$idx, 1980 ) );
+            $values[ 'contact_info' ] .= '#' . $idx;
         }
         return $values;
     }
