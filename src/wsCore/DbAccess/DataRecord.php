81a3adce3a09340a1ee8a9c40abfa66382fe4ef2
asao Kamei <asao@ubuPC.(none)>
2012-10-16 16:07:25 +0900

    added todo on getId.

--- src/wsCore/DbAccess/DataRecord.php
+++ src/wsCore/DbAccess/DataRecord.php
@@ -95,6 +95,7 @@ class DataRecord implements \ArrayAccess
     }
 
     /**
+     * TODO: this id thing looks pretty confusing, or buggy. 
      * @return mixed|null
      */
     public function getId() {
