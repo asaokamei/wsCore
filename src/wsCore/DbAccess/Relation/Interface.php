0caca6dbb229a1d149d974198bd8c068dc9e5896
asao Kamei <asao@ubuPC.(none)>
2012-10-16 16:04:23 +0900

    added PHPDoc for Relation_Interface.

--- src/wsCore/DbAccess/Relation/Interface.php
+++ src/wsCore/DbAccess/Relation/Interface.php
@@ -3,7 +3,20 @@ namespace wsCore\DbAccess;
 
 interface Relation_Interface
 {
+    /**
+     * @param DataRecord $target
+     * @return Relation_Interface
+     */
     public function set( $target );
-    public function del( $target );
+
+    /**
+     * @param DataRecord $target
+     * @return Relation_Interface
+     */
+    public function del( $target=null );
+
+    /**
+     * @return DataRecord
+     */
     public function get();
 }
