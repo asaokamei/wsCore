7150fe7b5444abdee911ac21c9a1c04f9ac33a23
asao Kamei <asao@ubuPC.(none)>
2012-10-19 15:23:56 +0900

    updated Dao_Network.

--- src/wsTests/DbAccess/Dao/Network.php
+++ src/wsTests/DbAccess/Dao/Network.php
@@ -1,45 +1,39 @@
 <?php
 namespace wsTests\DbAccess;
 
-class Dao_Group extends \wsCore\DbAccess\Dao
+class Dao_Network extends \wsCore\DbAccess\Dao
 {
     /** @var string     name of database table     */
-    protected $table = 'myGroup';
+    protected $table = 'network';
 
     /** @var string     name of primary key        */
-    protected $id_name = 'group_code';
+    protected $id_name = 'network_id';
 
     protected $definition = array(
-        'group_code'     => array( 'group code', 'number', ),
-        'group_name'   => array( 'name',         'string', ),
-        'new_dt_group' => array( 'created at',   'string', 'created_at'),
-        'mod_dt_group' => array( 'updated at',   'string', 'updated_at'),
+        'network_id'     => array( 'network code', 'number', ),
+        'friend_id_from' => array( 'my friend',    'number', ),
+        'friend_id_to'   => array( 'friend to',    'number', ),
+        'comment'        => array( 'comment',      'string', ),
+        'status'         => array( 'friendship',   'number', ),
+        'new_dt_network' => array( 'created at',   'string', 'created_at'),
+        'mod_dt_network' => array( 'updated at',   'string', 'updated_at'),
     );
 
     /** @var array      for validation of inputs       */
     protected $validators = array(
-        'group_code'   => array( 'number' ),
-        'group_name' => array( 'text', 'required' ),
+        'network_id'     => array( 'int',  'required' ),
+        'friend_id_from' => array( 'int',  'required' ),
+        'friend_id_to'   => array( 'int',  'required' ),
+        'comment'        => array( 'text', 'default:""',  ),
+        'status'         => array( 'int',  'required', ),
     );
 
     /** @var array      for selector construction      */
     protected $selectors  = array(
-        'group_code'   => array( 'Selector', 'text' ),
-        'group_name' => array( 'Selector', 'text', 'width:43' ),
+        'comment'        => array( 'Selector', 'textarea', 'ime:on' ),
+        'status'         => array( 'Selector', 'text',     'ime:off'),
     );
-    
-    protected $relations = array(
-        'friend' => array(
-            'relation_type' => 'HasJoined',
-            'join_table'         => 'friend2group', // same as the relation name
-            'join_source_column' => 'group_code', // same as the relation name
-            'join_target_column' => 'friend_id', // same as the relation name
-            'source_column' => 'group_code', // same as the relation name
-            'target_model'  => 'Dao_Friend',
-            'target_column' => 'friend_id', // use id.
-        ),
-    );
-    
+
     // +----------------------------------------------------------------------+
     /**
      * @param $query \wsCore\DbAccess\Query
@@ -56,8 +50,5 @@ class Dao_Group extends \wsCore\DbAccess\Dao
     }
     public function setSelector( $name, $info ) {
         $this->selectors[ $name ] = $info;
-    }
-    public function insert( $data ) {
-        return parent::insertValue( $data );
     }
 }
