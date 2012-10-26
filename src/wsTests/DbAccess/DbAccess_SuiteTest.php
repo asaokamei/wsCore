3cb049e4a5f51acc160bc122c3676c501ea4cfeb
asao Kamei <asao@ubuPC.(none)>
2012-10-26 15:41:46 +0900

    added the Query's quote test in the test suites.

--- src/wsTests/DbAccess/DbAccess_SuiteTest.php
+++ src/wsTests/DbAccess/DbAccess_SuiteTest.php
@@ -17,6 +17,7 @@ class DbAccess_SuiteTests
         $suite->addTestFile( $folder . 'Query_Test.php' );
         $suite->addTestFile( $folder . 'Query_MySql_Test.php' );
         $suite->addTestFile( $folder . 'Query_PgSql_Test.php' );
+        $suite->addTestFile( $folder . 'Query_PgSql_Quoted_Test.php' );
         $suite->addTestFile( $folder . 'Dao_MySql_Test.php' );
         $suite->addTestFile( $folder . 'DataRecord_MySql_Test.php' );
         $suite->addTestFile( $folder . 'Relation_MySql_Test.php' );
