adb40bf506d75e7730ae5cd68b8cbe84bb915265
Asao <kamei@workspot.jp>
2012-11-02 02:29:27 +0900

    uses EntityManager instead of directly using dao.

--- src/wsCore/DbAccess/Relation/HasRefs.php
+++ src/wsCore/DbAccess/Relation/HasRefs.php
@@ -6,11 +6,14 @@ namespace wsCore\DbAccess;
  */
 class Relation_HasRefs implements Relation_Interface
 {
-    /** @var DataRecord */
+    /** @var EntityManager */
+    protected $em;
+
+    /** @var Entity_Interface */
     protected $source;
     protected $sourceColumn;
 
-    /** @var DataRecord */
+    /** @var Entity_Interface */
     protected $target;
     /** @var Dao */
     protected $targetDao;
@@ -19,28 +22,30 @@ class Relation_HasRefs implements Relation_Interface
 
     protected $linked = false;
     /**
-     * @param DataRecord   $source
+     * @param EntityManager $em
+     * @param Entity_Interface   $source
      * @param array        $relInfo
      */
-    public function __construct( $source, $relInfo )
+    public function __construct( $em, $source, $relInfo )
     {
+        $this->em     = $em;
         $this->source = $source;
         $this->sourceColumn = isset( $relInfo[ 'source_column' ] ) ?
-            $relInfo[ 'source_column' ] : $source->getIdName() ;
+            $relInfo[ 'source_column' ] : $this->em->getModel( $source->_get_Model() )->getIdName() ;
         $this->targetModel  = $relInfo[ 'target_model' ];
         $this->targetColumn = ( isset( $relInfo[ 'target_column' ] ) ) ?
             $relInfo[ 'target_column' ] : $this->sourceColumn ;
-        $this->targetDao   = $this->source->getDao()->getInstance( $this->targetModel );
+        $this->targetDao   = $this->em->getModel( $this->targetModel );
     }
 
     /**
-     * @param DataRecord $target
+     * @param Entity_Interface $target
      * @return Relation_HasRefs
      * @throws \RuntimeException
      */
     public function set( $target )
     {
-        if( $target->getModel() != $this->targetModel ) {
+        if( $target->_get_Model() != $this->targetModel ) {
             throw new \RuntimeException( "target model not match!" );
         }
         $this->target = $target;
@@ -51,14 +56,14 @@ class Relation_HasRefs implements Relation_Interface
 
     /**
      * @param bool $save
-     * @return Relation_HasRefs|Relation_Interface
+     * @return Relation_Interface
      */
     public function link( $save=false )
     {
         if( $this->linked )  return $this;
         if( !$this->source ) return $this;
         if( !$this->target ) return $this;
-        if( $this->sourceColumn == $this->source->getIdName() &&
+        if( $this->sourceColumn == $this->em->getModel( $this->source->_get_Model() )->getIdName() &&
             !$this->source->isIdPermanent() ) {
             return $this;
         }
@@ -68,13 +73,13 @@ class Relation_HasRefs implements Relation_Interface
         $this->target->$column = $value;
         $this->linked = true;
         if( $save ) {
-            $this->target->save();
+            $this->em->save();
         }
         return $this;
     }
 
     /**
-     * @param DataRecord $target
+     * @param Entity_Interface $target
      * @return Relation_HasOne
      */
     public function del( $target=NULL ) {
@@ -86,16 +91,17 @@ class Relation_HasRefs implements Relation_Interface
     }
 
     /**
-     * @return array|DataRecord
+     * @return Entity_Interface[]
      */
     public function get()
     {
-        $value = $this->source->get( $this->sourceColumn );
+        $column = $this->sourceColumn;
+        $value = $this->source->$column;
         return $this->targetDao->query()->w( $this->targetColumn )->eq( $value )->select();
     }
 
     /**
-     * @return void|DataRecord[]
+     * @return void|Entity_Interface[]
      */
     public function getJoinRecord() {}
 
