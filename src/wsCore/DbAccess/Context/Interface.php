8c5360fd906888c588508643bed8fac692a2a35a
Asao <kamei@workspot.jp>
2012-11-07 04:40:57 +0900

    interface defines role should have register entity feature.

---
+++ src/wsCore/DbAccess/Context/Interface.php
@@ -0,0 +1,10 @@
+<?php
+namespace wsCore\DbAccess;
+
+interface Context_Interface
+{
+    /**
+     * @param \wsCore\DbAccess\Entity_Interface    $entity
+     */
+    public function register( $entity );
+}
