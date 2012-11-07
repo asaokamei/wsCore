e6525a765249e236a3853bc9373142979731c559
asao Kamei <asao@ubuPC.(none)>
2012-11-07 16:13:40 +0900

    some top page.

---
+++ public/index.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="./common/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="./common/css/bootstrap-responsive.css" />
+    <link rel="stylesheet" type="text/css" href="./common/css/main.css" />
+    <title>WScore Public Demo</title>
+    <style type="text/css">
+
+            /* Main marketing message and sign up button */
+        .jumbotron {
+            margin: 60px 0;
+            text-align: center;
+        }
+        .jumbotron h1 {
+            font-size: 60px;
+            line-height: 1;
+        }
+        .jumbotron .btn {
+            font-size: 21px;
+            padding: 14px 24px;
+        }
+    </style>
+</head>
+<body>
+<div class="container-narrow">
+    <div class="masthead">
+        <h3 class="muted">WScore Public Demo</h3>
+    </div>
+    <hr>
+    <header class="jumbotron">
+        <h1>WScore framework</h1>
+        <p class="lead">Data Mapper and DCI inspired web application framework.<br />
+            Wishes wishes wishes. </p>
+        <hr>
+    </header>
+    <h3>simple page without front controller</h3>
+    <ul>
+        <li><a href="password.php" >generate password</a></li>
+    </ul>
+    <footer class="footer">
+        <hr>
+        <p>WScore Developed by WorkSpot.JP<br />
+        thanks, bootstrap. </p>
+    </footer>
+</div>
+</body>
+</html>
