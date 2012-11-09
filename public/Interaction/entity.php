fad3804f602000eaeb4fa6a06523dedd9ab673fc
asao Kamei <asao@ubuPC.(none)>
2012-11-09 10:33:40 +0900

    added 3rd property, and removed unused (used as mock) properties.

--- public/Interaction/entity.php
+++ public/Interaction/entity.php
@@ -9,20 +9,8 @@ class entity extends \wsCore\DbAccess\Entity_Abstract
 
     public $friend_name = '';
 
+    public $friend_gender = '';
+
     public $friend_bday = null;
-
-    public $entityName = null;
-
-    public $role = null;
-
-    public $_actions = array();
-
-    public function loadData( $load ) {
-        $this->_actions[] = $load;
-    }
-    public function verify( $load ) {
-        $this->_actions[] = $load;
-        return !\wsCore\Utilities\Tools::getKey( $_REQUEST, 'error' );
-    }
 }
 
