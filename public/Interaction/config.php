8cafebc3a570a16f7ebd57bc6982c9608bd0a3fb
asao Kamei <asao@ubuPC.(none)>
2012-11-09 12:27:30 +0900

    moved interaction class as one file. renamed to config.php.

---
+++ public/Interaction/config.php
@@ -0,0 +1,25 @@
+<?php
+namespace Interaction;
+
+require_once( __DIR__ . '/model.php' );
+require_once( __DIR__ . '/entity.php' );
+require_once( __DIR__ . '/view.php' );
+require_once( __DIR__ . '/interaction.php' );
+
+class selGender extends \wsCore\Html\Selector
+{
+    // +----------------------------------------------------------------------+
+    /**
+     * @param \wsCore\Html\Form $form
+     * @DimInjection Fresh \wsCore\Html\Form
+     */
+    public function __construct( $form )
+    {
+        parent::__construct( $form );
+        $this->style = 'radio';
+        $this->item_data = array();
+        $this->item_data[] = array( 'M', 'Male' );
+        $this->item_data[] = array( 'F', 'Female' );
+    }
+}
+
