fcc20e2d859ff4551732c912498fef16127d8042
asao Kamei <asao@ubuPC.(none)>
2012-11-14 09:36:49 +0900

    moved menu at the H1 level.

--- public/task/views/template.php
+++ public/task/views/template.php
@@ -5,19 +5,19 @@ include( __DIR__ . '/../../common/menu/header.php' );
 
 /** @var $view \wsModule\Alt\Html\View_Bootstrap */
 $baseUrl = $view->get( 'baseUrl' );
+$taskUrl = $view->get( 'taskUrl' );
 
 ?>
-<h4>task/todo demo</h4>
-    <p>simple task/todo application for demonstrating data mapper, basic mvc. </p>
-    <style type="text/css">
-        ul.subMenu { clear: both; float: right;}
-        ul.subMenu li { float: left; list-style-type: none; padding: 0 1em; margin: 0 1px; background-color: #eef0f8; }
-    </style>
-<ul class="subMenu">
-    <li><a href="<?php echo $baseUrl; ?>myTasks/" >list tasks</a></li>
-    <li><a href="<?php echo $baseUrl; ?>myTasks/new" >new tasks</a></li>
-    <li><a href="<?php echo $baseUrl; ?>myTasks/setup" >setup</a></li>
+<style type="text/css">
+    ul.subMenu { clear: both; float: right;}
+</style>
+<ul class="nav nav-pills subMenu">
+    <li><a href="<?php echo $taskUrl; ?>" >My Tasks</a></li>
+    <li><a href="<?php echo $taskUrl; ?>new" >New Task</a></li>
+    <li><a href="<?php echo $taskUrl; ?>setup" >setup</a></li>
 </ul>
+<h4>task/todo demo</h4>
+<p>simple task/todo application for demonstrating data mapper, basic mvc. </p>
 <div style="clear:both">
 </div>
 <h1><?= $view->get( 'title' ); ?></h1>
