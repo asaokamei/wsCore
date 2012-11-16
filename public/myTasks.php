2ed4395601dd9ccd387c5ce44430b50e4e847fd3
asao Kamei <asao@ubuPC.(none)>
2012-11-16 14:58:32 +0900

    reformatted code.

--- public/myTasks.php
+++ public/myTasks.php
@@ -2,24 +2,25 @@
 include( __DIR__ . '/autoload.php' );
 include( __DIR__ . '/../src/autoloader.php' );
 use WScore\Core;
+
 Core::go();
 Core::setPdo( array( 'dsn' => 'sqlite:' . __DIR__ . '/task/data/tasks.sqlite' ) );
 
 /** @var $front wsModule\Alt\Web\FrontMC */
 Core::get( '\task\model\tasks' );
-$front = Core::get( '\wsModule\Alt\Web\FrontMC' );
-$front->debug = true;
+$front            = Core::get( '\wsModule\Alt\Web\FrontMC' );
+$front->debug     = true;
 $front->namespace = 'task';
-$routes = array(
-    'myTasks/printO/:name'  => array( 'controller' => 'task', 'action' => 'PrintO' ),
-    'myTasks/printO'  => array( 'controller' => 'task', 'action' => 'PrintO', 'name' => 'em' ),
-    'myTasks/setup'      => array( 'controller' => 'task', 'action' => 'setup' ),
-    'myTasks/new'      => array( 'controller' => 'task', 'action' => 'new' ),
-    'myTasks/done/:id'      => array( 'controller' => 'task', 'action' => 'done' ),
-    'myTasks/task/:id'      => array( 'controller' => 'task', 'action' => 'task' ),
+$routes           = array(
+    'myTasks/printO/:name' => array( 'controller' => 'task', 'action' => 'PrintO' ),
+    'myTasks/printO'       => array( 'controller' => 'task', 'action' => 'PrintO', 'name' => 'em' ),
+    'myTasks/setup'        => array( 'controller' => 'task', 'action' => 'setup' ),
+    'myTasks/new'          => array( 'controller' => 'task', 'action' => 'new' ),
+    'myTasks/done/:id'     => array( 'controller' => 'task', 'action' => 'done' ),
+    'myTasks/task/:id'     => array( 'controller' => 'task', 'action' => 'task' ),
     'myTasks/:action/:act' => array( 'controller' => 'task', 'act' => '' ),
-    'myTasks/' => array( 'controller' => 'task', 'action' => 'index' ),
-    'myTasks' => array( 'controller' => 'task', 'action' => 'index' ),
+    'myTasks/'             => array( 'controller' => 'task', 'action' => 'index' ),
+    'myTasks'              => array( 'controller' => 'task', 'action' => 'index' ),
 );
 $front->router->set( $routes );
 $front->run();
