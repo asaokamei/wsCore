df8cfc885a18739996d8b51f8ebbe8c3eecc316a
Asao <kamei@workspot.jp>
2012-11-25 19:22:29 +0900

    tentative class for Message.

---
+++ src/WScore/Validator/Message.php
@@ -0,0 +1,70 @@
+<?php
+namespace WScore\Validator;
+
+class Message
+{
+    /** @var array                  error message for some filters.  */
+    public $filterErrMsg = array();
+    
+    /** @var string                 error message for the value.     */
+    public $message = '';
+    
+    /** @var null                   type of value, if set.  */
+    public $type = null;
+    
+    /** @var array                  error message for each types.    */
+    public $typeErrMsg = array();
+    
+    public function __construct( $filter )
+    {
+        // error messages for each filter.
+        $this->filterErrMsg = array(
+            'encoding' => 'invalid encoding',
+            'required' => 'required field',
+            'sameAs'   => 'value not the same',
+            'sameEmpty' => 'missing value to compare',
+        );
+        // error messages for each type. 
+        $this->typeErrMsg = array(
+            'email' => 'invalid email',
+            'date'  => 'invalid date',
+        );
+    }
+    
+    public function __invoke( $message ) {
+        $this->setMessage( $message );
+        return $this;
+    }
+    public function setMessage( $message ) {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * @param array $error
+     * @return string
+     */
+    public function message( $error )
+    {
+        $message = '';
+        $rule    = null;
+        if( empty( $error ) ) return $message;
+        foreach( $error as $rule => $option ) {}
+        if( !$rule ) return $message;
+        
+        // search for filter specific error message
+        if( isset( $this->filterErrMsg[ $rule ] ) ) {
+            return $this->filterErrMsg[ $rule ];
+        }
+        // message for this specific value. 
+        if( isset( $this->message ) ) {
+            return $this->message;
+        }
+        // message for this type, if type is set. 
+        if( isset( $this->type ) && isset( $this->typeErrMsg[ $this->type ] ) ) {
+            return $this->typeErrMsg[ $this->type ];
+        }
+        return "invalid input";
+    }
+    
+}
