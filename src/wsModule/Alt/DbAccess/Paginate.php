a1410e84e243e9cd0ada3296dd7ab4e6b55f4364
asao Kamei <asao@ubuPC.(none)>
2012-12-12 15:39:52 +0900

    rewrote Paginate for DbAccess pagination.

--- src/wsModule/Alt/DbAccess/Paginate.php
+++ src/wsModule/Alt/DbAccess/Paginate.php
@@ -2,267 +2,279 @@
 namespace wsModule\Alt\DbAccess;
 
 /**
- *	Pagination class for SQL.
- *
- *	@copyright     Copyright 2010-2011, Asao Kamei
- *	@link          http://www.workspot.jp/cena/
- *	@license       GPLv2
+ * @property $limit
+ * @property $offset
+ * @property $total
+ * @property $per_page
+ * @property $num_links
+ * @property $page_url
+ * @property $curr_page
+ * @property $top_page
+ * @property $last_page
+ * @property $next_page
+ * @property $prev_page
+ * @property $pages
+ * @property $options
  */
-if( !defined( "WORDY" ) ) define( "WORDY",  0 ); // very wordy...
-
-/* ============================================================================ */
-function pn_disp_all( &$pn, $url=NULL )
+class Paginate implements \ArrayAccess
 {
-    if( WORDY ) echo "<br>pn_disp_all( $pn, $url )<br>\n";
-    if( WORDY > 3 ) wordy_table( $pn, 'pn' );
-    if( !$url ) {
-        $url = htmlspecialchars( $_SERVER['PHP_SELF'] );
+    protected $data = array(
+        'limit'     => null,
+        'offset'    => 0,
+        'total'     => 0,
+        'per_page'  => 10,
+        'num_links' => 5,
+        'page_url'  => null,
+        'curr_page' => 1,
+        'top_page'  => null,
+        'last_page' => null,
+        'next_page' => null,
+        'prev_page' => null,
+        'pages'     => array(),
+        'options'   => array(),
+    );
+    protected $url = array(
+        'pages' => array(),
+    );
+    public $page_parameter = 'page';
+    protected $curr_page = null;
+
+    // +----------------------------------------------------------------------+
+    //  constructor and setup pagination information.
+    // +----------------------------------------------------------------------+
+    /**
+     *
+     */
+    public function __construct() {
     }
-    echo pn_disp_link( $pn, $url, 'top',  '<<' );
-    echo ' ';
-    echo pn_disp_link( $pn, $url, 'prev', 'prev' );
-    echo ' ';
-    pn_disp_pages( $pn, $url );
-    echo pn_disp_link( $pn, $url, 'next', 'next' );
-    echo ' ';
-    echo pn_disp_link( $pn, $url, 'last', '>>' );
-}
 
-/* ============================================================================ */
-function pn_disp_pages( &$pn, $url, $word=NULL )
-{
-    if( !empty( $pn['page'] ) ) {
-        foreach( $pn['page'] as $key=>$arg ) {
-            if( empty( $arg ) ) {
-                $link = "<strong>{$key}</strong>&nbsp;";
+    /**
+     * @param int $total
+     * @return Paginate
+     */
+    public function setTotal( $total ) {
+        $this->total = $total;
+        return $this;
+    }
+
+    /**
+     * @param int $page
+     * @return Paginate
+     */
+    public function setCurrPage( $page ) {
+        $this->curr_page = $page;
+        return $this;
+    }
+
+    /**
+     * @param $option
+     * @return Paginate
+     */
+    public function setOptions( $option )
+    {
+        if( !$this->curr_page && isset( $option[ $this->page_parameter ] ) ) {
+            $this->curr_page = $option[ $this->page_parameter ];
+            unset( $option[ $this->page_parameter ] );
+        }
+        $this->options = $option;
+        return $this;
+    }
+    // +----------------------------------------------------------------------+
+    // +----------------------------------------------------------------------+
+    /**
+     * @return Paginate
+     */
+    public function setupPageOption() {
+        $this->options[ $this->page_parameter ] = $this->curr_page;
+        return $this;
+    }
+    /**
+     * @param null|string $url
+     * @return array
+     */
+    public function setupUrls( $url=null )
+    {
+        if( $url ) $this->page_url = $url;
+        $properties = array( 'top_page', 'last_page', 'next_page', 'prev_page' );
+        foreach( $properties as $prop ) {
+            $this->url[ $prop ] = $this->makeUrl( $this->$prop );
+        }
+        foreach( $this->pages as $key => $page ) {
+            $this->url[ 'pages' ][ $key ] = $this->makeUrl( $page );
+        }
+        return $this->url;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function url( $name ) {
+        if( is_numeric( $name ) ) return $this->url[ 'pages' ][ $name ];
+        return $this->url[ $name ];
+    }
+
+    /**
+     * @param $page
+     * @return string
+     */
+    public function makeUrl( $page )
+    {
+        if( !$page ) return '';
+        $url = sprintf( $this->page_url, $page );
+        if( !empty( $this->options ) ) {
+            $url .= '?';
+            foreach( $this->options as $key => $val ) {
+                $key = htmlentities( $key, ENT_QUOTES );
+                $val = urlencode( $val );
+                $url .= strpos( $url, -1 ) == '?' ? '' : '&';
+                $url .= "{$key}={$val}";
             }
-            else {
-                $link = "<a href='{$url}?{$arg}'>{$key}</a>&nbsp;";
-            }
-            echo $link;
         }
-    }
-}
-
-/* ============================================================================ */
-function pn_disp_link( &$pn, $url, $type, $word=NULL, $disp=TRUE )
-{
-    if( WORDY > 4 ) echo " --disp_pv_link( &$pn, $url, $type, $word )-- \n";
-    if( !$word ) $word = ucfirst( strtolower( $type ) );
-
-    $args = $pn[ $type ];
-    if( empty( $args ) ) {
-        if( $disp ) {
-            $link = "{$word}";
-        }
-        else {
-            $link = NULL;
-        }
-    }
-    else {
-        $link = "<a href='{$url}?{$args}'>{$word}</a>";
-    }
-    return $link;
-}
-
-/* ============================================================================ */
-class Dba_Page
-{
-    const  MAX_LIST = 20;
-    static $default_limit   = 10; // rows per page. overwrite Dba_Page::$limit=5. 
-
-    var $limit    = NULL;
-    var $sql      = NULL; // sql module 
-    var $start      =  0; // offset count
-    var $count      =  0; // total number of data -> should fetch everytime.
-
-    var $options    = ''; // other form inputs to construct where condition.
-    var $max_page   =  0; // maximum number of page used inside this class
-
-    // +--------------------------------------------------------------- +
-    function __construct( $sql, $opt=array() )
-    {
-        if( WORDY ) echo "<b>created instance of paginate</b>...<br>\n";
-        $this->sql = $sql;
-        if( have_value( $opt, 'options' ) ) $this->setOptions( $opt[ 'options' ] );
-        $this->getParameters( $opt );
-    }
-    // +--------------------------------------------------------------- +
-    function setPage()
-    {
-        $this->sql
-            ->limit( $this->limit )
-            ->offset( $this->start );
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function fetchPage( &$data )
-    {
-        $this->setPage();
-        $this->sql
-            ->execSelect()
-            ->fetchAll( $data );
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function fetchPN( &$pn ) {
-        $pn = $this->_getPrevNext();
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function setOptions( $options ) {
-        // to set options used to construct sql where condition.
-        if( WORDY ) echo "<br>setOptions( $options )<br>\n";
-        if( WORDY > 3 ) print_r( $options );
-        $this->options = $options;
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function getParameters( $data=array() )
-    {
-        if( is_array( $data ) ) $data = $data + $_REQUEST;
-        $this->start = have_value( $data, 'start' ) ? $data[ 'start' ] : 0;
-        $this->limit = have_value( $data, 'limit' ) ? $data[ 'limit' ] : self::$default_limit;
-        // no count supported. count everytime.
-        //$this->count = is_numeric( $data[ 'count' ] ) ? $data[ 'count' ] : $this->count;
-        $this->_setMaxPage();
-        if( WORDY ) {
-            echo "got parameters: " .
-                "start={$this->start}, limit={$this->limit}, count={$this->count}, " .
-                "max={$this->max_page}<br> ";
-        }
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function _setMaxPage()
-    {
-        if( $this->count == 0 ) {
-            $this->_setCount();
-        }
-        if( $this->count == 0 || $this->limit == 0 ) {
-            $this->max_page = 0;
-        }
-        else {
-            $this->max_page = ceil( $this->count/$this->limit );
-        }
-        if( WORDY ) echo "_setMaxPage = {$this->max_page}<br>\n";
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function _setCount()
-    {
-        $this->sql->fetchCount( $this->count );
-        if( WORDY ) echo "_setCount = {$this->count}<br>\n";
-        return $this;
-    }
-    // +--------------------------------------------------------------- +
-    function _getPrevNext()
-    {
-        // total found
-        $pg_info['start']  = $this->start;
-        $pg_info['limit']  = $this->limit;
-        $pg_info['count']  = $this->count;
-
-        // make NEXT button
-        $start = $this->start + $this->limit;
-        $pg_info[ "next" ] = $this->_getURL( $start );
-
-        // make PREV button
-        $start = $this->start - $this->limit;
-        $pg_info[ "prev" ] = $this->_getURL( $start );
-
-        // make TOP button
-        if( $this->max_page > 1 ) {
-            $pg_info[ "top" ] = $this->_getURL( 0 );
-        }
-        else
-            $pg_info[ 'top' ] = '';
-
-        // make LAST button
-        if( $this->start != ( $this->max_page - 1 ) * $this->limit ) {
-            $pg_info[ "last" ] = $this->_getURL( ( $this->max_page - 1 ) * $this->limit );
-        }
-        else
-            $pg_info[ 'last' ] = '';
-
-        // make PAGEs 
-        if( $this->max_page < self::MAX_LIST * 2 ) {
-            $pg_add = $this->getPrevNext1();
-        }
-        else {
-            $pg_add =$this->getPrevNext2();
-        }
-        $pg_info = array_merge( $pg_info, $pg_add );
-
-        return $pg_info;
-    }
-    // +--------------------------------------------------------------- +
-    function getPrevNext2()
-    {
-        if( WORDY ) echo "<b>getPrevNext2()</b>...<br>\n";
-        $pg_info    = array();
-        $repeat     = self::MAX_LIST * 2;
-        $curr_page  = floor( $this->start / $this->limit ) + 1; // page starts from 1...
-        $max_offset = $this->max_page - $repeat + 1;
-
-        // getting the first page
-        if( $curr_page > self::MAX_LIST ) {
-            $page_start = $curr_page - self::MAX_LIST;
-        }
-        else {
-            $page_start = 1;
-        }
-        if( $page_start > $max_offset ) $page_start = $max_offset;
-
-        // make PAGE numbers
-        for( $i = 0; $i < $repeat; $i++ ) {
-            $page  = $page_start + $i;
-            $start = $i * $this->limit;
-            $pg_info['page'][ $page ] = $this->_getURL( $start );
-        }
-
-
-        if( WORDY > 4 ) wordy_table( $pg_info, 'getPrevNext2' );
-        return $pg_info;
-    }
-    // +--------------------------------------------------------------- +
-    function getPrevNext1()
-    {
-        if( WORDY ) echo "<b>getPrevNext1()</b>...<br>\n";
-        $pg_info = array();
-
-        // make PAGE numbers
-        for( $i = 0; $i < $this->max_page; $i++ ) {
-            $page_id = $i + 1;
-            $start   = $i * $this->limit;
-            $pg_info['page'][ $page_id ] = $this->_getURL( $start );
-        }
-        if( WORDY > 4 ) wordy_table( $pg_info, 'getPrevNext1'. is_array( $pg_info ) );
-        return $pg_info;
-    }
-    // +--------------------------------------------------------------- +
-    function _getURL( $start )
-    {
-        if( WORDY > 4 ) echo "_makeURL( $start )...<br>\n";
-        $url   = '';
-        if( $start > $this->count ) {
-            // no url
-        }
-        else
-            if( $start < 0 ) {
-                // what's wrong ? no url
-            }
-            else
-                if( $start == $this->start ) {
-                    // current page. no url
-                }
-                else {
-                    $url = "start={$start}&limit={$this->limit}";
-                    // $url .= "&count={$this->count}"; // no adding count.
-                    if( $this->options ) $url .= "&{$this->options}";
-                }
         return $url;
     }
-    // +--------------------------------------------------------------- +
-}
+    // +----------------------------------------------------------------------+
+    // +----------------------------------------------------------------------+
+    /**
+     * @param \WScore\DbAccess\Query $query
+     */
+    public function setQuery( $query )
+    {
+        $this->calc();
+        $query->offset( $this->getOffset() );
+        $query->limit(  $this->getLimit() );
+    }
+    /**
+     * @return int
+     */
+    public function getOffset() {
+        return (integer) ( ( $this->curr_page - 1 ) * $this->per_page );
+    }
 
+    /**
+     * @return int
+     */
+    public function getLimit() {
+        return $this->per_page;
+    }
+
+    /**
+     *
+     */
+    public function calc()
+    {
+        // check the current page.
+        if( !$this->curr_page ) $this->curr_page = 1;
+        // setting top page.
+        $this->top_page = 1;
+        $start = $this->curr_page - $this->num_links;
+        if( $start <= 1 ) {
+            $start = 1;
+            $this->top_page = null;
+        }
+        // setting last page.
+        if( !$this->total ) { // in case total is not set...
+            $this->last_page = null;
+            $last = $this->num_links + $this->curr_page; // show pages up to the num_links.
+        }
+        else {
+            $this->last_page = (integer) ( ceil( $this->total / $this->per_page ) + 1 );
+            $last = $this->curr_page + $this->num_links;
+            if( $this->last_page <= $last ) {
+                $last = $this->last_page;
+                $this->last_page = null;
+            }
+        }
+        // create pages
+        for( $page = $start; $page <= $last; $page ++ ) {
+            if( $page == $this->curr_page ) {
+                $this->pages[ $page ] = '';
+            }
+            else {
+                $this->pages[ $page ] = $page;
+            }
+        }
+        if( $this->curr_page > 1 ) $this->prev_page = $this->curr_page - 1;
+        if( $this->curr_page != $last ) $this->next_page = $this->curr_page + 1;
+    }
+    // +----------------------------------------------------------------------+
+    //  magic methods and ArrayAccess methods.
+    // +----------------------------------------------------------------------+
+    /**
+     * @param $offset
+     * @return mixed
+     */
+    public function __get( $offset ) {
+        return $this->offsetGet( $offset );
+    }
+
+    /**
+     * @param $offset
+     * @param $value
+     */
+    public function __set( $offset, $value ) {
+        $this->offsetSet( $offset, $value );
+    }
+    /**
+     * @param mixed $offset  An offset to check for.
+     * @return boolean true on success or false on failure.
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists( $offset ) {
+        return isset( $this->data[ $offset ] );
+    }
+
+    /**
+     * @param mixed $offset  The offset to retrieve.
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet( $offset ) {
+        return isset( $this->data[ $offset ] ) ? $this->data[ $offset ] : null;
+    }
+
+    /**
+     * @param mixed $offset  The offset to assign the value to.
+     * @param mixed $value   The value to set.
+     * @return void
+     */
+    public function offsetSet( $offset, $value ) {
+        $this->data[ $offset ] = $value;
+    }
+
+    /**
+     * @param mixed $offset
+     * @return void
+     */
+    public function offsetUnset( $offset ) {
+        if ( isset( $this->data[ $offset ] ) ) unset( $this->data[ $offset ] );
+    }
+}
+/*
+
+example of generating pagination ul > li using Tags.
+well, not tested yet.
+
+$pages = new Paginate(); // and som settings.
+$pages->setTotal( 123 );
+$pages->setCurrPage( 5 );
+$pages->calc();
+$pages->setupUrls( '/path/to/%d' ); // %d replaces the page number.
+$tags  = new \WScore\Html\Tags();
+
+$ul = $tags->ul();
+if( $pages->prev_page ) $ul->contain_( $tags->li( $tags->a( 'Prev' )->href( $pages->url( 'prev_page' ) ) ) );
+if( $pages->top_page ) $ul->contain_( $tags->li( $tags->a( $pages->top_page ) )->href( $pages->url( 'top_page')) );
+foreach( $pages->pages as $page => $value ) {
+    if( $value ) {
+        $ul->contain_( $tags->li( $tags->a( $page )->href( $pages->url( $page ) ) )->_class( 'active' ) );
+    }
+    else {
+        $ul->contain_( $tags->li( $page )->_class( 'disabled' ) );
+    }
+}
+if( $pages->last_page ) $ul->contain_( $tags->li( $tags->a( $pages->last_page )->href( $pages->url( 'last_page' ) ) ) );
+if( $pages->next_page ) $ul->contain_( $tags->li( $tags->a( 'Next' )->href( $pages->url( 'next_page' ) ) ) );
+echo $paginate = $tags->div( $ul )->_class( 'pagination' );
+
+
+ */
