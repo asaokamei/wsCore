0b0ae357e2aeb21960db179b1c2fd02c813f9baf
Asao <kamei@workspot.jp>
2013-01-11 07:06:57 +0900

    load links (relations). fixed EntityManager as service.

--- src/WScore/DataMapper/Role/CenaLoad.php
+++ src/WScore/DataMapper/Role/CenaLoad.php
@@ -9,7 +9,7 @@ class Role_CenaLoad extends Role_Loadable
      * @param \WScore\DataMapper\EntityManager    $em
      * @param \WScore\Validator\DataIO            $dio
      * @param \WScore\DataMapper\CenaManager      $cena
-     * @DimInjection Get \WScore\DataMapper\EntityManager
+     * @DimInjection Get EntityManager
      * @DimInjection Get \WScore\Validator\DataIO
      * @DimInjection Get \WScore\DataMapper\CenaManager
      */
@@ -31,7 +31,18 @@ class Role_CenaLoad extends Role_Loadable
         if( empty( $data ) ) $data = $_POST;
         $data = $this->cena->getDataForCenaId( $data, $this->entity->_get_cenaId() );
         parent::loadData( $name, $data[ 'prop' ] );
-        // TODO: implement relation.
+        return $this;
+    }
+    public function loadLink( $method='set', $data=array() )
+    {
+        if( is_array(  $method ) ) $data = $method;
+        if( empty( $data ) ) $data = $_POST;
+        $data = $this->cena->getDataForCenaId( $data, $this->entity->_get_cenaId() );
+        if( empty( $data[ 'link' ] ) ) return $this;
+        foreach( $data[ 'link' ] as $name => $link ) {
+            $entities = $this->cena->getCenaEntity( $link );
+            $this->em->relation( $this->entity, $name )->$method( $entities );
+        }
         return $this;
     }
 }
