f904b447e5b858825697c3674e4d4abe43265e53
Asao <kamei@workspot.jp>
2013-01-13 15:43:43 +0900

    pool the relations objects in Relation object.

--- src/WScore/DataMapper/Relation.php
+++ src/WScore/DataMapper/Relation.php
@@ -4,7 +4,7 @@ namespace WScore\DataMapper;
 class Relation
 {
     /** @var Relation_Interface[]      pools relation objects.             */
-    static $pool = array();
+    private $relations = array();
 
     /**
      * @param EntityManager     $em
@@ -15,14 +15,18 @@ class Relation
      * @throws \RuntimeException
      * @return Relation_Interface
      */
-    static public function getRelation( $em, $source, $relations, $name, $type=NULL )
+    public function getRelation( $em, $source, $relations, $name, $type=null )
     {
         if( empty( $relations ) ) {
             throw new \RuntimeException( "no relations. " );
         }
-        $relation = NULL;
+        $relation = null;
         foreach( $relations as $relName => $relInfo ) {
-            if( $relName == $name ) {
+            if( $relName == $name ) 
+            {
+                if( $relation = $this->checkRelation( $source, $name ) ) {
+                    return $relation;
+                }
                 $relInfo[ 'relation_name' ] = $name;
                 $type = $relInfo[ 'relation_type' ];
                 $class = '\WScore\DataMapper\Relation_' . ucwords( $type );
@@ -30,6 +34,7 @@ class Relation
                     throw new \RuntimeException( "no relation class for $class" );
                 }
                 $relation = new $class( $em, $source, $relInfo );
+                $this->saveRelation( $source, $name, $relation );
             }
         }
         if( is_null( $relation ) ) {
@@ -38,6 +43,28 @@ class Relation
         return $relation;
     }
 
+    /**
+     * @param Entity_Interface $entity
+     * @param string $name
+     * @param Relation_Interface $relation
+     */
+    private function saveRelation( $entity, $name, $relation ) {
+        $cenaId = $entity->_get_cenaId();
+        $this->relations[ $cenaId ][ $name ] = $relation;
+    }
+    /**
+     * @param Entity_Interface $entity
+     * @param string $name
+     * @return null|Relation_Interface
+     */
+    private function checkRelation( $entity, $name )
+    {
+        $cenaId = $entity->_get_cenaId();
+        if( isset( $this->relations[ $cenaId ][ $name ] ) ) {
+            return $this->relations[ $cenaId ][ $name ];
+        }
+        return null;
+    }
     /**
      * TODO: implement late-linking relations.
      * to relate unsaved DataRecord, run this method after all the DataRecords
