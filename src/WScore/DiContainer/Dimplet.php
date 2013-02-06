3253f1e7f01e3a3ad209ed8738c3895e39d19146
Asao <kamei@workspot.jp>
2013-02-07 07:53:22 +0900

    moved array_merge... to Utils class.

--- src/WScore/DiContainer/Dimplet.php
+++ src/WScore/DiContainer/Dimplet.php
@@ -106,7 +106,7 @@ class Dimplet
     public function fresh( $id, $option=array() )
     {
         if( isset( $this->options[$id] ) ) {
-            $option = $this->array_merge_recursive_distinct( $this->options[$id], $option );
+            $option = Utils::array_merge_recursive_distinct( $this->options[$id], $option );
         }
         if( $this->values->exists( $id ) ) 
         {
@@ -151,7 +151,7 @@ class Dimplet
     public function construct( $className, $option=array() )
     {
         $injectList = $this->forge->listDi( $className );
-        $injectList = $this->array_merge_recursive_distinct( $injectList, $option );
+        $injectList = Utils::array_merge_recursive_distinct( $injectList, $option );
         foreach( $injectList['construct'] as $key => $injectInfo ) {
             $injectList['construct'][$key] = $this->getObject( $injectInfo );
         }
@@ -242,52 +242,5 @@ class Dimplet
         else {
             $this->extends[$id] = $callable;
         }
-    }
-
-    /**
-     * FROM:
-     * http://www.php.net/manual/ja/function.array-merge-recursive.php#92195
-     * 
-     * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
-     * keys to arrays rather than overwriting the value in the first array with the duplicate
-     * value in the second array, as array_merge does. I.e., with array_merge_recursive,
-     * this happens (documented behavior):
-     *
-     * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
-     *     => array('key' => array('org value', 'new value'));
-     *
-     * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
-     * Matching keys' values in the second array overwrite those in the first array, as is the
-     * case with array_merge, i.e.:
-     *
-     * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
-     *     => array('key' => array('new value'));
-     *
-     * Parameters are passed by reference, though only for performance reasons. They're not
-     * altered by this function.
-     *
-     * @param array $array1
-     * @param array $array2
-     * @return array
-     * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
-     * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
-     */
-    private function array_merge_recursive_distinct ( array &$array1, array &$array2 )
-    {
-        $merged = $array1;
-
-        foreach ( $array2 as $key => &$value )
-        {
-            if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
-            {
-                $merged [$key] = $this->array_merge_recursive_distinct ( $merged [$key], $value );
-            }
-            else
-            {
-                $merged [$key] = $value;
-            }
-        }
-
-        return $merged;
     }
 }
