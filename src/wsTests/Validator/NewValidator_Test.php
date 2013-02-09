ed47497975057bcba713ee6c04bba41d7f50b6d2
Asao <kamei@workspot.jp>
2013-02-09 18:11:13 +0900

    fixed for new NewValidator api.

--- src/wsTests/Validator/NewValidator_Test.php
+++ src/wsTests/Validator/NewValidator_Test.php
@@ -4,14 +4,19 @@ require_once( __DIR__ . '/../../autoloader.php' );
 
 use \WScore\Core;
 
-class DataIO_Test extends \PHPUnit_Framework_TestCase
+class NewValidator_Test extends \PHPUnit_Framework_TestCase
 {
-    /** @var \WScore\Validator\DataIO */
+    /** @var \WScore\Validator\NewValidator */
     var $dio;
+    
+    /** @var \WScore\Validator\Rules */
+    var $rule;
+    
     public function setUp()
     {
         Core::go();
-        $this->dio = Core::get( 'DataIO' );
+        $this->dio  = Core::get( '\WScore\Validator\NewValidator' );
+        $this->rule = Core::get( '\WScore\Validator\Rules' );
     }
     public function getData()
     {
@@ -21,16 +26,34 @@ class DataIO_Test extends \PHPUnit_Framework_TestCase
         );
     }
     // +----------------------------------------------------------------------+
+    public function test_simple_invalid()
+    {
+        // todo: fix Validate
+        $err_msg = 'Not a Number';
+        $data = $this->getData();
+        $this->dio->source( $data );
+        $this->dio->push( 'email', $this->rule->number( 'message:'.$err_msg ) );
+        $isError = $this->dio->isValid();
+        $errors  = $this->dio->popError();
+        $value   = $this->dio->pop();
+
+        $this->assertEquals( $data[ 'email' ], $value[ 'email' ] );
+        $this->assertFalse( $isError );
+        $this->assertEquals( $err_msg, $errors['email'] );
+    }
     public function test_validating_array()
     {
+        // todo: fix Validate
         $input = array( 'num' => array( '1', '2', 'bad', '4' ) );
         $err_msg = 'Not a Number';
-        $errors = array();
         $this->dio->source( $input );
-        $this->dio->pushValue( 'num', 'pattern:number | err_msg:'.$err_msg );
+        $rule = $this->rule->number( 'message:'.$err_msg );
+        $this->dio->push( 'num', $rule );
         // check errors.
-        $isError = $this->dio->popErrors( $errors );
-        $this->assertTrue( !!$isError );
+        $isError = $this->dio->isValid();
+        $errors  = $this->dio->popError();
+        
+        $this->assertFalse( $isError );
         $this->assertNotEmpty( $err_msg, $errors['num'][2] );
         $this->assertEquals( $err_msg, $errors['num'][2] );
 
@@ -46,17 +69,26 @@ class DataIO_Test extends \PHPUnit_Framework_TestCase
     public function test_simple_push_and_pop()
     {
         $data = $this->getData();
-        $value = NULL;
         $this->dio->source( $data );
-        $this->dio->pushValue( 'number', 'matches:number', $value );
+        $this->dio->push( 'number', $this->rule->number() );
+        $value = $this->dio->pop();
 
-        $this->assertEquals( $data[ 'number' ], $value );
+        $this->assertEquals( $data[ 'number' ], $value[ 'number' ] );
+    }
+    public function test_simple_push_mail()
+    {
+        $data = $this->getData();
+        $this->dio->source( $data );
+        $this->dio->push( 'email', $this->rule->email() );
+        $value = $this->dio->pop();
+
+        $this->assertEquals( $data[ 'email' ], $value[ 'email' ] );
     }
     public function test_multiple()
     {
         $input = array( 'date_y' => '1981', 'date_m' => '08', 'date_d' => '18' );
         $this->dio->source( $input );
-        $this->dio->push( 'date', 'date' );
+        $this->dio->push( 'date', $this->rule->date() );
         
         $date = $this->dio->pop( 'date' );
         $this->assertEquals( '1981-08-18', $date );
