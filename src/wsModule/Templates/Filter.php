1e95937dc13a6c2309a9f3213aa6dbf6c0a1d529
asao Kamei <asao@ubuPC.(none)>
2013-02-12 13:56:32 +0900

    created a Filter object to handle all filter objects.

---
+++ src/wsModule/Templates/Filter.php
@@ -0,0 +1,59 @@
+<?php
+namespace wsModule\Templates;
+
+class Filter
+{
+    /** @var array */
+    protected $filters = array();
+
+    /**
+     * @param object $basic
+     * @param object $date
+     * @DimInjection \wsModule\Templates\Filter_Basic
+     * @DimInjection \wsModule\Templates\Filter_Date
+     */
+    public function __construct( $basic, $date )
+    {
+        $this->filters[ 'basic' ] = $basic;
+        $this->filters[ 'date'  ] = $date;
+    }
+
+    /**
+     * @param string $name
+     * @param object $filter
+     */
+    public function setFilter( $name, $filter ) {
+        $this->filters[ $name ] = $filter;
+    }
+
+    /**
+     * @param string   $value
+     * @param string[] $filters
+     * @param string   $method
+     * @return mixed
+     */
+    protected function apply( $value, $filters, $method='' )
+    {
+        // check if $method maybe a filter name in basic filters.
+        if( $method && method_exists( $this->filters['basic'], $method ) ) {
+            $value = $this->filters['basic']->$method( $value );
+        }
+        if( empty( $filters ) ) return $value;
+        // setup which filter object to use.
+        $objects = array( $this->filters['basic'] ); // always use basic filters.
+        if( $method && isset( $this->filters[ $method ] ) ) {
+            $objects[] = $this->filters[ $method ];  // use additional filters.
+        }
+        // apply filters
+        foreach( $filters as $f ) {
+            foreach( $this->filters as $obj ) {
+                if( method_exists( $obj, $f ) ) {
+                    $value = $obj->$f( $value );
+                    break;
+                }
+            }
+        }
+        return $value;
+    }
+
+}
